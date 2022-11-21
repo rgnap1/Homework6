f62f7db39a96cb65c4edb06f9bc098a55ea24151
rgnap <osovskaya_1993@mail.ru>
2022-11-21 17:32:27 +0300

    Commit 2. Finished hw part 2.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Homework6.1 ");
+        System.out.println("task1 ");
+        //task1
+        for(int i = 1; i < 11; i++){
+            System.out.println("Итерация цикла " + i );
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("task2 ");
+        //task2
+        for (int i = 10; i > 0; i--){
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("task3 ");
+        //task3
+        for (int i = 0 ; i < 18; i = i + 2) {
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("task4 ");
+        //task4
+        for (int i = 10; i > -11; i--){
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println(" ");
+        System.out.println("Homework6.2 ");
+        System.out.println("task1 ");
+        //task1
+        for(int i = 1904; i < 2097; i = i+4){
+            System.out.println(i + " год является високосным");
+        }
+        System.out.println(" ");
+        System.out.println("task2 ");
+        //task2
+        for(int i = 7; i < 99; i = i+7){
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println(" ");
+        System.out.println("task3 ");
+        //task3
+        for(int i = 1; i < 513; i = i*2){
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println(" ");
+        System.out.println("Homework6.3 ");
+        System.out.println("task1 ");
+        //task1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
