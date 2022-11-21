842a53022bf648e8dd1c28265fea6b3d6f18d636
rgnap <osovskaya_1993@mail.ru>
2022-11-21 18:12:14 +0300

    Finished hw

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(" ");
-        System.out.println("Homework6.2 ");
+        System.out.println("Homework6.3 ");
         System.out.println("task1 ");
         //task1
-        for(int i = 1904; i < 2097; i = i+4){
-            System.out.println(i + " год является високосным");
-        }
+        int moneyboxInMounth = 29000;
+        int totalMoney = 0;
+        for (int i = 1; i < 13; i++){
+            totalMoney = moneyboxInMounth*12;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + moneyboxInMounth + " рублей");
+        } System.out.println("За год накопилось вего " + totalMoney + " рублей" );
+
         System.out.println(" ");
         System.out.println("task2 ");
         //task2
-        for(int i = 7; i < 99; i = i+7){
-            System.out.println("Итерация цикла " + i);
-        }
-        System.out.println(" ");
-        System.out.println("task3 ");
-        //task3
-        for(int i = 1; i < 513; i = i*2){
-            System.out.println("Итерация цикла " + i);
-        }
+        int moneyboxInMounth1 = 29000;
+        int totalMoney1;
+        int totalMoney2 = 0;
+        for (int i = 1; i < 13; i++){
+            totalMoney1 = moneyboxInMounth1/100;
+            totalMoney1 = totalMoney1+moneyboxInMounth1;
+            totalMoney2 = totalMoney1*12;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + totalMoney1 + " рублей");
+        } System.out.println("За год накоплений под 12% годовых скопилось вего " + totalMoney2 + " рублей" );
 
 
 
